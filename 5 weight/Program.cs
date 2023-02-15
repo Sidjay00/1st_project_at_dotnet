4ed2c3a155d17c43389dd05696d32b1828d0782d
K.Sidorov <2887458@gmail.com>
2023-02-15 23:36:43 +0300

    add 5 weight

---
+++ 5 weight/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 1; // new Random().Next(1, 11);
+int b = 8; // new Random().Next(1, 11);
+int c = 3; // new Random().Next(1, 11);
+int d = 2; // new Random().Next(1, 11);
+int e = 6; // new Random().Next(1, 11);
+Console.Write(a + ", ");
+Console.Write(b + ", ");
+Console.Write(c + ", ");
+Console.Write(d + ", ");
+Console.WriteLine(e + ".");
+
+int max = a;
+
+Console.WriteLine(max);
+
+if (b > max)
+    {
+        max = b;
+    }
+else if (c > max);
+    {
+        max = c;
+    }
+    break;
+
+Console.WriteLine(max);
