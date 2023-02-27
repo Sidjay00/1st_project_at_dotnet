7c21baa0f32b035624668630434903e027152b3e
K.Sidorov <2887458@gmail.com>
2023-02-27 22:08:32 +0300

    added second void method

--- Example012_Method/Program.cs
+++ Example012_Method/Program.cs
@@ -4,4 +4,25 @@ void Method1()
     System.Console.WriteLine("Автор Константин");
 }
 
-Method1();
+// Method1();
+
+// Вид 2
+void Method2(string msg)
+{
+    System.Console.WriteLine(msg);
+}
+
+// Method2("Текст сообщения!");
+
+void Method2_1(string msg, int count)
+{
+    int i = 0;
+    while(i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+
+Method2_1(msg: "Текст!", count: 4);
+Method2_1(count: 2, msg: "Новый текст!");
