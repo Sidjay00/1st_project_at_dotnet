65961a2a6383dd4924879c1df7ee4b349022a573
K.Sidorov <2887458@gmail.com>
2023-02-27 22:42:54 +0300

    added multiplication table

--- Example012_Method/Program.cs
+++ Example012_Method/Program.cs
@@ -17,7 +17,7 @@ void Method2(string msg)
 void Method2_1(string msg, int count)
 {
     int i = 0;
-    while(i < count)
+    while (i < count)
     {
         Console.WriteLine(msg);
         i++;
@@ -44,17 +44,43 @@ int year = Method3();
 // Вид 4
 
 
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = string.Empty;
+//     while(i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "k");
+// Console.WriteLine(res);
+
+// Цикл for на базе 4-го метода
+
 string Method4(int count, string text)
 {
-    int i = 0;
     string result = string.Empty;
-    while(i < count)
+    for (int i = 0; i < count; i++)
     {
         result = result + text;
-        i++;
     }
     return result;
 }
 
 string res = Method4(10, "k");
-Console.WriteLine(res);
+// Console.WriteLine(res);
+
+// Цикл в цикле
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
