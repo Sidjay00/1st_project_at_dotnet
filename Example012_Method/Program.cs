2ae160dd5ebff50c426785831314ea59bfae859f
K.Sidorov <2887458@gmail.com>
2023-02-27 22:21:01 +0300

    added method 4

--- Example012_Method/Program.cs
+++ Example012_Method/Program.cs
@@ -44,3 +44,17 @@ int year = Method3();
 // Вид 4
 
 
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = string.Empty;
+    while(i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "k");
+Console.WriteLine(res);
